0d633e2e53b1c9be1704db01d6236a9e10a7fde3
BrianKan99 <81730589+BrianKan99@users.noreply.github.com>
2023-11-16 15:38:18 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+?>
+
+     <div class="dropdown">
+        <button class="dropbtn">主頁</a></button>
+        <div class="dropdown-content">
+            <a href="index.php">學校傑出作品欣賞</a>
+          </div>
+    </div>
+
+<div class="dropdown">
+        <button class="dropbtn">最新消息</a></button>
+        <div class="dropdown-content">
+            <a href="List of new works.php">新作品列表</a>
+            <a href="ArtNews.php">美術新聞</a>
+          </div>
+    </div>
+
+
+    <div class="dropdown">
+        <button class="dropbtn"><a>作品資訊</a></button>
+        <div class="dropdown-content">
+            <a href="Artworks ranking.php">作品排行榜</a>
+            <a href="Student testimonials.php">學生感言</a>
+            <a href="Artworks album.php">作品相簿</a>
+          </div>
+    </div>
+
+    <div class="dropdown">
+        <button class="dropbtn">登錄/註冊</a></button> 
+        <div class="dropdown-content">
+            <a href="Login account.php?op=login">登錄帳戶</a>
+            <a href="Register.php">進行註冊</a>
+          </div>
+    </div>
+        <div class="dropdown">
+        <button class="dropbtn">關於我們</a></button>
+        <div class="dropdown-content">
+        <a href="about us.php">關於我們(詳細)</a>
+        <a href="common problem.php">常見問題</a>
+        <a href="contact us.php">聯繫我們</a>
+        </div>
+      </div>
+
+      <?php 
+    if ($_SESSION)
+    {
+        echo '<a href="/functions.php?op=logout">登出</a>';
+    }
+    ?>
+      <div class="slideshow-container">
+        <img class="mySlides" src="images/cefabc6aa7bd28cccd0713e4a3656cc9.jpg" style="width:100%;">
+        <img class="mySlides" src="images/BmWXkgF.jpeg" style="width:100%">
+        <img class="mySlides" src="images/sF0IA8u.jpeg" style="width:100%">
+      </div>
+    </div>    
