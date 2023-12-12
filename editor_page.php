593bbb487950d076bc2260f8664173eef6d9c2e0
BrianKan99 <81730589+BrianKan99@users.noreply.github.com>
2023-12-12 12:27:26 +0800

    Add files via upload

--- editor_page.php
+++ editor_page.php
@@ -19,7 +19,7 @@ if(!isset($_SESSION['editor_name'])){
          padding: 0;
          font-family: montserrat;
          background: linear-gradient(120deg,#2980b9, #8e44ad);
-         height: 100vh;
+         height: 150vh;
          overflow: hidden;    
          } 
 
@@ -49,6 +49,12 @@ if(!isset($_SESSION['editor_name'])){
          right: -730px;
 
         }
+
+        .pmain{
+         position: absolute;
+         top: 370px;
+         right: -100px;
+        }
 </style>
     <header>
     <meta charset="UTF-8">
@@ -58,12 +64,10 @@ if(!isset($_SESSION['editor_name'])){
     <link rel="stylesheet" href="css/all.css">  
 </head>
 <body>
+
         <a href="index.php"><img src="images/hkit-removebg-preview.png" width="90" height="90"></a><strong class="animate-charcter">編輯者頁面</strong>
-        </section>
       </header>
-
       <?php include('header.php'); ?>
-
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
 
@@ -74,15 +78,23 @@ if(!isset($_SESSION['editor_name'])){
 
    <div class="content">
       <h2>歡迎登入<span>編輯者頁面</span></h2>
-      <h1>welcome <span><?php echo $_SESSION['editor_name'] ?></span></h1>
+      <h1>Welcome <span><?php echo $_SESSION['editor_name'] ?></span></h1>
       <a class = "button"href="display.php" class="button button1">檢視學生作品</a>
       <a class = "button" href="Artworks ranking.php" class="button button1">作品排行榜</a>
       <a class = "button" href="logout.php" class="button button1">logout</a>
    </div>
 
-</div>
+   <p class = "pmain">
+   網站編輯者的更新網站職責可能包括：<br><br>
+1. 編輯和校對網站上的內容，確保其準確性、一致性和流暢性。<br><br>
+2. 負責更新和維護網站的內容，包括文字、圖片、視頻等。<br><br>
+3. 確保網站的結構和導航系統清晰易用，以提供良好的用戶體驗。<br><br>
+4. 處理網站上的用戶反饋和查詢，回答問題並提供支持。<br><br>
+5. 監測網站的性能和流量，進行分析並提出改進建議。<br><br>
+6. 跟蹤和應用最新的網站設計和內容管理系統的趨勢和最佳實踐。<br><br>
+</p>
 
 </body>
-<br><br><br><br><br><br><br><br><br> <br><br><br> <br><br><br> <br><br><br> <br><br><br> <br><br><br>
+<br><br><br><br><br><br><br><br><br> <br><br><br> <br><br><br> <br><br><br> <br><br><br> <br><br>
 <?php include('footer.php'); ?>
 </html>
